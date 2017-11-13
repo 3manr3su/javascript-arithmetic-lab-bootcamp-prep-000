fc812df7374f295f50f9711a249113c3039b0f35
andy mcghie <mcghiea@gmail.com>
2017-11-13 08:49:55 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function inc(n){
+  return (n+1)
+}
+function dec(n){
+  return (n - 1)
+}
+function add (a, b) {
+  return (a + b) 
+}
+
+function subtract(a, b) {
+  return (a - b)
+}
+
+function multiply(a, b) {
+  return(a * b)
+}
+
+funciton divide(a, b) {
+  return (a / b)
+}
+
+
+function makeInt(a) {
+  return parseInt(a)
+  }
+
+function preserveDecimal(a) {
+  return parseFloat(a)
+  }
+
